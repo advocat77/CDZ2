4f286439a1e37364742467a38e92320dca4be528
Alex <advocat6787428@gmail.com>
2023-07-11 21:04:15 +0200

    task1 run

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите трехзначное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int firstDigit = number / 100;
+int thirdDigit = number % 10;
+int result = (number - firstDigit * 100 - thirdDigit) / 10;
+Console.WriteLine($"Вторая цифра числа {number}->{result}");
