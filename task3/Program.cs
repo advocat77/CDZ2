7f99efd95e3dba50722eb87f6ce12436665ed7ab
Alex <advocat6787428@gmail.com>
2023-07-11 21:22:12 +0200

    task3 run

---
+++ task3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите цифру, обозначающую день недели ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 6)
+{
+   Console.WriteLine($"Сегодня выходной");
+}
+else
+if (number == 7)
+{
+   Console.WriteLine($"Сегодня выходной");
+}
+else
+   Console.WriteLine($"Сегодня рабочий день");
