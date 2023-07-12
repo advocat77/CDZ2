c7b6b964626463e2b46838c2c705a73817f10afb
Alex <advocat6787428@gmail.com>
2023-07-12 20:19:48 +0200

    task2 finished

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100)
+{
+   Console.WriteLine($"Третьей цифры нет");
+}
+else
+if (number > 1000)
+{
+   number = number / 10;
+}
+int thirdDigit = number % 10;
+Console.WriteLine($"Третья цифра числа {thirdDigit}");
